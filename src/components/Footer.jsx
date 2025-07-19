464ecbf2feaa191879ca0fec53550e642f400b36
Brent Ogden <ogden87@gmail.com>
2025-07-19 16:53:52 -0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,9 +16,9 @@ export default function Footer() {
                         alt="B Squared Solutions"
                         className="h-26 w-auto items-center px-25 md:h-30 mb-4 md:px-0 md:w-auto"
                     />
-                    <p className="text-md text-white pr-20 text-center md:text-left">
+                    {/* <p className="text-md text-white pr-20 text-center md:text-left">
                         Your go‑to digital growth partner—offering bespoke React & Tailwind websites, strategic technical consulting, data‑driven SEO & marketing, and dedicated ongoing support to power your online success.
-                    </p>
+                    </p> */}
                 </div>
 
                 {/* Explore links */}
@@ -37,9 +37,9 @@ export default function Footer() {
                     <div className='ml-20 md:ml-0'>
                         <h4 className="text-white text-xl font-semibold mb-2 text-shadow-lg text-shadow-gray-700/30">Follow Us</h4>
                         <div className="flex space-x-4 text-white">
-                            <a href="https://twitter.com/MileHiRocks5280" aria-label="Twitter" className="hover:text-gray-700" target='_blank'><FaTwitter size={30} /></a>
-                            <a href="https://youtube.com/@MileHiMayhem303" aria-label="YouTube" className="hover:text-gray-700" target='_blank'><FaYoutube size={30} /></a>
-                            <a href="https://instagram.com/MileHiRocks5280" aria-label="Instagram" className="hover:text-gray-700" target='_blank'><FaInstagram size={30} /></a>
+                            <a href="#" aria-label="Twitter" className="hover:text-gray-700" target='_blank'><FaTwitter size={30} /></a>
+                            <a href="#" aria-label="YouTube" className="hover:text-gray-700" target='_blank'><FaYoutube size={30} /></a>
+                            <a href="#" aria-label="Instagram" className="hover:text-gray-700" target='_blank'><FaInstagram size={30} /></a>
                         </div>
                     </div>
                 </div>
