dfa41f7c63f39883e4efdd02f18e437c20cf5dbc
Brent Ogden <ogden87@gmail.com>
2025-08-16 00:25:45 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
@@ -30,7 +30,7 @@ export default function App() {
   return (
     <Router>
       <div className="flex flex-col overflow-x-hidden min-h-screen">
-        <Navbar />
+        <NavBar />
         <ScrollManager />
         <HashScroll />
 
